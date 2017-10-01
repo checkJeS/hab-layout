03c2af4dbb91996e6097f0877394e6c757d5c4a3
checkJeS <lopezjesica38@gmail.com>
2017-09-30 18:41:26 -0700

    Add files via upload

---
+++ Creator.java
@@ -0,0 +1,14 @@
+package com.example.bepo_lite.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Creator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creator);
+
+    }
+}
